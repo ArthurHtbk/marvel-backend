2fceeadc979f9af1f1a31d58638df9ffca900ce4
ArthurHtbk <arthur.heurtebise@gmail.com>
2021-08-08 22:56:17 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome!" });
+});
+
+app.get("/comics/:characterId", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics/${req.params.characterId}?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+    // console.log(response.data);
+    res.json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.get("/comics", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}&skip=${req.query.skip}&title=${req.query.title}`
+    );
+    // console.log(response.data);
+    res.json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.get("/characters", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}&skip=${req.query.skip}&name=${req.query.name}`
+    );
+    // console.log(response.data);
+    res.json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "Page not found" });
+});
+
+app.listen(4000, () => {
+  console.log("Server started!");
+});
