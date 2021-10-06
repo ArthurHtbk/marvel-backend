b6aa0ffc991712dd442d08edab863ce8214adaec
ArthurHtbk <arthur.heurtebise@gmail.com>
2021-10-06 14:11:49 +0100

    add signup, login and middleware

--- index.js
+++ index.js
@@ -1,14 +1,93 @@
+require("dotenv").config();
 const express = require("express");
+const formidable = require("express-formidable");
+const mongoose = require("mongoose");
 const axios = require("axios");
 const cors = require("cors");
+const SHA256 = require("crypto-js/sha256");
+const encBase64 = require("crypto-js/enc-base64");
+const uid2 = require("uid2");
+const cloudinary = require("cloudinary").v2;
 
-require("dotenv").config();
+const User = require("./models/User");
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
 
 const app = express();
+app.use(formidable());
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome!" });
+});
+
+app.post("/user/signup", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.fields.email });
+    if (!user) {
+      const salt = uid2(16);
+      const hash = SHA256(req.fields.password + salt).toString(encBase64);
+      const token = uid2(16);
+
+      const newUser = new User({
+        email: req.fields.email,
+        username: req.fields.username,
+        token: token,
+        hash: hash,
+        salt: salt,
+      });
+
+      const result = await cloudinary.uploader.upload(req.files.picture.path, {
+        folder: `/marvel/user/${newUser._id}`,
+      });
+
+      newUser.picture = result;
+      await newUser.save();
+      res.status(200).json(newUser);
+    } else {
+      res
+        .status(409)
+        .json("An account has already been created for this email address");
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.post("/user/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.fields.email });
+
+    if (user) {
+      const newHash = SHA256(req.fields.password + user.salt).toString(
+        encBase64
+      );
+
+      console.log(newHash);
+
+      if (newHash === user.hash) {
+        res.status(200).json(user);
+      } else {
+        res.status(401).json("Incorrect email address and / or password");
+      }
+    } else {
+      res.status(401).json("Incorrect email address and / or password");
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 app.get("/comics/:characterId", async (req, res) => {
---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.model("User", {
+  email: {
+    unique: true,
+    type: String,
+  },
+  username: {
+    required: true,
+    type: String,
+  },
+  picture: Object,
+  token: String,
+  hash: String,
+  salt: String,
+});
+
+module.exports = User;
